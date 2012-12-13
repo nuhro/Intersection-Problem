f2b90f2372fca11dc630b547e6016625616406d6
Ralfk <ralf@kohrt.at>
2012-12-13 19:23:44 +0100

    fixed next_ij for cars which 'want' to go left

--- code/crosslight_next_ij.m
+++ code/crosslight_next_ij.m
@@ -4,79 +4,79 @@ function [ ni, nj ] = crosslight_next_ij(i, j, direction,EXIT_LEFT ,EXIT_RIGHT ,
 
 switch(direction)
     case EXIT_LEFT
-        if(i == 1 && j == 4)
-            ni = 2;
-            nj = 4;
-        elseif(i == 2 && j == 4)
-            ni = 3;
-            nj = 3;
-        elseif(i == 3 && j == 3)
-            ni = 4;
-            nj = 2;
-        elseif(i == 4 && j == 2)
-            ni = 5;
-            nj = 1;
-        elseif(i == 5 && j == 1)
-            ni = -4;
-            nj = 1;
-        elseif(i == 3 && j == 1)
-            ni = 3;
-            nj = 2;
-        elseif(i == 3 && j == 2)
-            ni = 4;
-            nj = 3;
-        elseif(i == 4 && j == 3)
-            ni = 5;
-            nj = 4;
-        elseif(i == 5 && j == 4)
-            ni = 6;
-            nj = 5;
-        elseif(i == 6 && j == 5)
-            ni = -3;
-            nj = 1;
-        elseif(i == 4 && j == 6)
-            ni = 4;
-            nj = 5;
-        elseif(i == 4 && j == 5)
-            ni = 3;
-            nj = 4;
-        elseif(i == 3 && j == 4)
+        if(i == 1 && j == 3)
             ni = 2;
             nj = 3;
         elseif(i == 2 && j == 3)
-            ni = 1;
+            ni = 3;
+            nj = 4;
+        elseif(i == 3 && j == 4)
+            ni = 4;
+            nj = 5;
+        elseif(i == 4 && j == 5)
+            ni = 5;
+            nj = 6;
+        elseif(i == 5 && j == 6)
+            ni = -4;
+            nj = 1;
+        elseif(i == 4 && j == 1)
+            ni = 4;
             nj = 2;
-        elseif(i == 1 && j == 2)
+        elseif(i == 4 && j == 2)
+            ni = 3;
+            nj = 3;
+        elseif(i == 3 && j == 3)
+            ni = 2;
+            nj = 4;
+        elseif(i == 2 && j == 4)
+            ni = 1;
+            nj = 5;
+        elseif(i == 1 && j == 5)
             ni = -1;
             nj = 1;
-        elseif(i == 6 && j == 3)
+        elseif(i == 6 && j == 4)
             ni = 5;
-            nj = 3;
-        elseif(i == 5 && j == 3)
-            ni = 4;
             nj = 4;
-        elseif(i == 4 && j == 4)
+        elseif(i == 5 && j == 4)
+            ni = 4;
+            nj = 3;
+        elseif(i == 4 && j == 3)
+            ni = 3;
+            nj = 2;
+        elseif(i == 3 && j == 2)
+            ni = 2;
+            nj = 1;
+        elseif(i == 2 && j == 1)
+            ni = -2;
+            nj = 1;
+        elseif(i == 3 && j == 6)
             ni = 3;
             nj = 5;
         elseif(i == 3 && j == 5)
-            ni = 2;
-            nj = 6;
-        elseif(i == 2 && j == 6)
-            ni = -2;
+            ni = 4;
+            nj = 4;
+        elseif(i == 4 && j == 4)
+            ni = 5;
+            nj = 3;
+        elseif(i == 5 && j == 3)
+            ni = 6;
+            nj = 2;
+        elseif(i == 6 && j == 2)
+            ni = -3;
             nj = 1;
         elseif(i < 0)   %here I assume the car is in the last position of the inmwards street
             if(i == -1)
                 ni = 1;
-                nj = 4;
+                nj = 3;
             elseif(i == -2)
                 ni = 4;
-                nj = 6;
+                nj = 1;
             elseif(i == -3)
                 ni = 6;
-                nj = 3;
+                nj = 4;
             elseif(i == -4)
                 ni = 3;
-                nj = 1;
+                nj = 6;
             end
         end
     case EXIT_RIGHT
